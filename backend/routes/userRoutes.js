9cc33ab9079c6c9819cce69ad521974f6b8654b2
Madhur Mittal <madhurmittal55@gmail.com>
2024-06-20 17:08:06 +0530

    changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,6 @@ const auth = require('../middleware/auth');
 const { users, deleteUser, editUser } = require('../Controllers/UserController');
 const router = express.Router();
 
-
 router.get('/', users)
 router.put('/:id', editUser);
 router.delete('/:id', auth, deleteUser);
